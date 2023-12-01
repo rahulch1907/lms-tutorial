a1164ed25ba91d27fd68d0ab0576ed190ba61f03
Chinta Rahul Reddy <rahulch1907@gmail.com>
2023-12-01 09:38:12 +0530

    Added full text search and index to Course model (generated by blackbox.ai )

---
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -0,0 +1,23 @@
+import { Chapter, Course, UserProgress } from "@prisma/client";
+
+import { NavbarRoutes } from "@/components/navbar-routes";
+
+import { CourseMobileSidebar } from "./course-mobile-sidebar";
+
+interface CourseNavbarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+export const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
+  return (
+    <div className="flex items-center h-full p-4 bg-white border-b shadow-sm">
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
+      <NavbarRoutes />
+    </div>
+  );
+};
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -13,7 +13,7 @@ export const NavbarRoutes = () => {
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
-    const isPlayerPage = pathname?.includes("/chapter");
+    const isPlayerPage = pathname?.includes("/courses");
     const isSearchPage = pathname === "/search";
 
     return (
